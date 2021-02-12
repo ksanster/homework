49b513213c694defae5ab41c95dcce9143395658
Alexander Syomin <a.semin@corp.mail.ru>
2021-02-12 11:42:50 +0300

    Д/з к уроку 7

---
+++ Lesson7/Ex1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Ex1
+{
+    public partial class MainForm : Form
+    {
+        private int operand = 0;
+        private int guessed = 0;
+        private int step = 0;
+        private Random rnd = new Random();
+        private Stack<int> stack = new Stack<int>();
+
+        public int Operand
+        {
+            get => operand;
+            set
+            {
+                operand = value;
+                //Правильнее использовать data binding, но пока не до конца с ним разобрался
+                lblValue.Text = operand.ToString();
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Operand = 0;
+        }
+
+        private void GameStart()
+        {
+            guessed = rnd.Next(1, 100);
+            step = 0;
+            Operand = 0;
+            MessageBox.Show($"Получите число {guessed}");
+        }
+
+        private void Clear()
+        {
+            stack.Clear();
+            Operand = 0;
+            guessed = 0;
+            step = 0;
+        }
+
+        private void CheckGameEnd()
+        {
+            if (guessed == 0)
+                return;
+
+            step++;
+            if (Operand == guessed)
+            {
+                MessageBox.Show($"Вы выиграли! Потребовалось ходов: {step}");
+                Clear();
+                return;
+            }
+            if (Operand > guessed)
+            {
+                MessageBox.Show($"Вы проиграли! Потребовалось ходов: {step}");
+                Clear();
+                return;
+            }
+        }
+
+        private void menuPlay_Click(object sender, EventArgs e)
+        {
+            GameStart();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            stack.Push(Operand);
+            Operand++;
+            CheckGameEnd();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            stack.Push(Operand);
+            Operand *= 2;
+            CheckGameEnd();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (stack.Count == 0)
+                return;
+
+            if (step > 0)
+                step--;
+
+            Operand = stack.Pop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+    }
+}
---
+++ Lesson7/Ex2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ex2
+{
+    public partial class MainForm : Form
+    {
+        private int guessed = 0;
+        private int step = 0;
+        private Random rnd = new Random();
+
+        public int Operand  
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(inputBox.Text))
+                    return 0;
+                if (!int.TryParse(inputBox.Text, out var result))
+                {
+                    inputBox.Text = string.Empty;
+                    return 0;
+                }
+                return result;
+
+            }
+        }
+
+            
+        public MainForm()
+        {
+            InitializeComponent();
+            GameStart();
+        }
+        private void Clear()
+        {
+            step = 0;
+            guessed = 0;
+            lblTryResult.Text = string.Empty;
+            inputBox.Text = string.Empty;
+            btnInput.Enabled = true;
+            inputBox.Enabled = true;
+        }
+
+        private void GameStart()
+        {
+            Clear();
+            guessed = rnd.Next(1, 100);
+        }
+
+        private void CheckGameEnd()
+        {
+            if (Operand < 1 || Operand > 100)
+            {
+                lblTryResult.Text = "Ошибка! Введите число от 1 до 100";
+                inputBox.Text = string.Empty;
+                return;
+            }
+             step++;
+            if (Operand == guessed)
+            {
+                btnInput.Enabled = false;
+                inputBox.Enabled = false;
+                lblTryResult.Text = $"Вы выиграли! Потребовалось ходов: {step}";
+                return;
+            }
+            lblTryResult.Text = (Operand > guessed) ? "Перебор!" : "Недобор!";
+        }
+
+        private void inputBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                CheckGameEnd();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            CheckGameEnd();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            GameStart();
+        }
+    }
+}
